099f3d974e1290b1c6a2069b9f751c0abcb51d5b
Astrid González <130100068+AstridPaola-GonzalezDiaz@users.noreply.github.com>
2023-05-08 15:41:12 -0600

    Update RandomStoryGenerator.py

--- RandomStoryGenerator.py
+++ RandomStoryGenerator.py
@@ -1,2 +1,4 @@
 # Importing random module
 import random
+## Creación de vector de iniciación de historia
+Sentence_starter = ['Erase una vez', ' En un mundo muy lejano', 'En el comienzo de los tiempos', 'Hace 100 años']
