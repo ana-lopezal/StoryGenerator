2ab84999ce67e3a153bfb55eca7db655aff0dac5
ArantzaGIM <127063148+ArantzaGIM@users.noreply.github.com>
2023-05-08 16:13:48 -0600

    Update RandomStoryGenerator.py
    Cambios de redacción final

--- RandomStoryGenerator.py
+++ RandomStoryGenerator.py
@@ -5,27 +5,27 @@ Sentence_starter = ['Erase una vez', ' En un mundo muy lejano', 'En el comienzo 
 
 ## Creación de vector donde especifica el personaje
 character = [' vivía un Rey.',' había un panadero llamado Jack.',
-             ' vivía un granjero.', ' José, un caballero']
+             ' vivía un granjero.', ' existía José, un caballero.']
 
 ## Creación de vector que indica el tiempo del día en el cual se lleva acabo la historia.
 time = [' En un día normal por la mañana', ' En una noche fría y solitaria']
 
 ## Creación de vector que indica hacia donde va el personaje
-story_plot = [' Iba caminando hacia el lugar para ', ' Iba a comer en un lugar mágico hacia']
+story_plot = [' iba caminando hacia', ' iba a comer en un lugar mágico hacia']
 
 ## Creación de vector que especifica lugar
 place = [' las montañas', ' el jardín', ' el castillo']
 
  ## Creación de vector que especifica personaje secundario 
-second_character = [ ' vió un caballo', ' vió una princesa', ' vió un perro']
+second_character = [ ' cuando vió un caballo', ' cuando vió una princesa', ' cuando vió un perro']
 
 ## Creación de vector de edad del personaje secundario
-age = [',que parecía no tener más de 25 años,', ',que se veía de una edad bastante avanzada,', 
-       ',que se veía como si nunca hubiera descansado en su vida,', ',que parecía estar en una edad madura,' ]
+age = [', que parecía no tener más de 25 años,', ', que se veía de una edad bastante avanzada,', 
+       ', que se veía como si nunca hubiera descansado en su vida,', ', que parecía estar en una edad madura,' ]
 
 ## Creación de vector con la acción del personaje secundario
-work = [' buscando algo con mucho esmero.', 'cavando un gran pozo.', 'corriendo de la forma más veloz que hubiera visto jamás.', 
-        'saltar sin control.']
+work = [' buscando algo con mucho esmero.', ' cavando un gran pozo.', ' corriendo de la forma más veloz que hubiera visto jamás.', 
+        ' saltar sin control.']
 
 ## Print de la historia
 print(random.choice(Sentence_starter)+random.choice(character)+
