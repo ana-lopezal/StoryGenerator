c7d9d34baf5992f0a5f361f6ad01f22aaf6929c4
Astrid González <130100068+AstridPaola-GonzalezDiaz@users.noreply.github.com>
2023-05-08 15:42:01 -0600

    Update RandomStoryGenerator.py

--- RandomStoryGenerator.py
+++ RandomStoryGenerator.py
@@ -2,3 +2,7 @@
 import random
 ## Creación de vector de iniciación de historia
 Sentence_starter = ['Erase una vez', ' En un mundo muy lejano', 'En el comienzo de los tiempos', 'Hace 100 años']
+
+## Creación de vector donde especifica el personaje
+character = [' vivía un Rey.',' había un panadero llamado Jack.',
+             ' vivía un granjero.', ' José, un caballero']
